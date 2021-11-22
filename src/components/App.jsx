779759ddcb97f5d7b53eeb6aa7e190889236695e
nazaninrezaei88@gmail.com <nazaninrezaei88@gmail.com>
2021-11-22 10:20:21 -0500

    create conditional style for submit page

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClicked() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMouseOver === true ? "black" : "white" }}
+        onClick={handleClicked}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
